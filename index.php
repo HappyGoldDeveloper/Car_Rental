fbbb3fc6ff2a955f4443793417bf6e076fe9781c
arsh98000 <arsh98000@gmail.com>
2019-03-22 19:07:47 -0500

    Redesigned index page with bootstrap.

--- index.php
+++ index.php
@@ -16,66 +16,78 @@ if(isset($_SESSION['admin']))
 <head>
 	<link rel="stylesheet" type="text/css" href="css/index.css">
 	<title>F.A.L.T.U Rentals</title>
-	<style type="text/css">
-			header
-			{
-				background-color: #000000;
-			}
-			#logo
-			{
-				border-radius: 100px;
-				height: 200px;
-
-			}
-			body
-			{
-				color: rgb(213, 218, 226);				
-			}
-
-	</style>
+	
 </head>
 <body>
-<header>
+<!-- <header>
 	<img id="logo" src="images/logo.jpg" alt="logo">
-</header>
+</header> -->
 
-<?php if (!isset($_SESSION['admin'])): ?>
-<p>
-	<a href="login.php">[login]</a>
-</p>
-<?php endif; ?>
+<header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">FALTU RENTALS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-<?php if (isset($_SESSION['admin'])): ?>
-<p>
-	<a href="logout.php">[logout]</a>
-</p>
-<?php endif; ?>
-	<?php if (!(isset($_SESSION['admin']))): ?>
-	<a href="signup.php">Sign up</a>
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="rides.php">Rides</a>
+      </li>
+	<?php if (isset($_SESSION['admin'])): ?>
+		<?php if ($admin == 1): ?>	
+		<li class="nav-item">
+		<a class="nav-link" href="create.php">Manage</a>
+		</li>
+		<?php endif; ?>
 	<?php endif; ?>
 
-<div>
-	<nav>
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="rides.php">Rides</a></li>
-			<?php if (isset($_SESSION['admin'])): ?>
-				<?php if ($admin == 1): ?>
-			<li><a href="create.php">Add New Ride</a></li>
-				<?php endif; ?>
-			<?php endif; ?>
-			<li><a href="contact.php">Contact</a></li>
-		</ul>
-	</nav>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+    	<?php if (!isset($_SESSION['admin'])): ?>
+		<a class="nav-link" href="login.php">Sign In</a>
+	<?php endif; ?>
+	<?php if (isset($_SESSION['admin'])): ?>
+		<a class="nav-link" href="logout.php">[logout]</a>
+	<?php endif; ?>
+	<?php if (!(isset($_SESSION['admin']))): ?>
+		<a class="nav-link" href="signup.php">Sign up</a>
+	<?php endif; ?>
+
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+</header>
+
+<div class="jumbotron">
+  <h1 class="display-3">Hi Car Lovers!</h1>
+  <p class="lead">Our inventory consists of all, Sports, Classic, Supercars and many more.</p>
+  <hr class="my-4">
+  <p>World class rides available.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="rides.php" role="button">Checkout Our Rides!</a>
+  </p>
 </div>
-<section>
-	<div>
-		
-	</div>
-</section>
-<footer>
-	
-</footer>
+
+<!-- Footer -->
+<footer class="page-footer font-small special-color-dark pt-4">
+
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="index.php"> Faltu.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
 </body>
 </html>
 
--- session.php
+++ session.php
@@ -3,6 +3,8 @@
    global $db;
    session_start();
 
+   
+
    if (isset($_SESSION['userId']))
    {
       $session_userId = $_SESSION['userId'];
@@ -17,3 +19,6 @@
       $_SESSION['username'] =$row['username'];
    }
 ?>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
